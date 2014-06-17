4a3b83745ebef91d245ed5944bacf554ded0296e
pattyland <mail@pattyland.de>
2014-06-17 22:04:39 +0200

    Added custom.sample.css

--- info.php
+++ info.php
@@ -3,7 +3,7 @@
 $template_directory			= 'madoven';
 $template_name				= 'Madoven';
 $template_function			= 'template';
-$template_version			= '1.1.1';
+$template_version			= '1.2';
 $template_platform			= '2.8';
 $template_author			= 'pattyland';
 $template_license			= 'MIT';
